bfd1c4681584711cf05f335c861c594119ee8d8c
Vince Lam <57117625+vince-lam@users.noreply.github.com>
2026-06-29 09:20:13 +0800

    chore: extract web UI to separate repo

--- web/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  darkMode: "class",
-  theme: { extend: {} },
-  plugins: [],
-} satisfies Config;
